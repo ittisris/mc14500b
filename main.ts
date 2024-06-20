76f243dc8a33d1662fbb26d47f464b9f531840e2
Itti Srisumalai <i2ktel@hotmail.com>
2024-06-20 20:35:28 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_PIN_
     basic.pause(100)
     led.unplot(0, 0)
 })
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_RISE, function () {
+    basic.showNumber(pc)
+})
 input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     basic.pause(500)
@@ -10,16 +13,21 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(500)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
-    basic.showNumber(pc)
     pc = pc + 1
     if (pc > 9) {
         pc = 0
     }
 })
 let pc = 0
+led.setBrightness(100)
 pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
 pc = 0
 basic.forever(function () {
-    basic.pause(500)
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        led.plot(4, 0)
+    } else {
+        led.unplot(4, 0)
+    }
+    basic.pause(100)
 })

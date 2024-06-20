2e92cc14ed69ec5b5655a1c82f25efb0f098af3c
Itti Srisumalai <i2ktel@hotmail.com>
2024-06-20 18:01:50 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
-input.onPinPressed(TouchPin.P0, function () {
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_PIN_EVT_RISE, function () {
     led.plot(0, 0)
-})
-input.onPinReleased(TouchPin.P0, function () {
+    basic.pause(100)
     led.unplot(0, 0)
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showNumber(pc)
+    pc = pc + 1
+    if (pc > 9) {
+        pc = 0
+    }
     pins.digitalWritePin(DigitalPin.P1, 1)
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P1, 0)
     basic.pause(500)
 })
-pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
+let pc = 0
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+pc = 0
 basic.forever(function () {
-	
+    basic.pause(500)
 })
